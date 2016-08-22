70ec01ea29136fe8d31977dbef59c32f239ee39c
Atahualpa S. Kraemer <ata.kraemer@gmail.com>
2016-08-22 11:33:00 -0500

    Create Titles_a_buscarse_en_google.mk

---
+++ Bibliograf-a/Titles_a_buscarse_en_google.mk
@@ -0,0 +1,7 @@
+1. The worm problem of Leo Moser.
+- Brownian dynamics with hydrodynamic interactions.
+- Polymer simulations companion:an introduction to brownian dynamics
+- Magnetotransport of sinai billiard
+- Diffusion in the lorentz gas
+
+...
